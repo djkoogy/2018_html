f23dea6f5ab129b4da0a0fe3ec4f96df6566216b
B. Kook Ha <djkoogy@gmail.com>
2018-04-16 21:13:10 +0000

    주정중

--- action.php
+++ action.php
@@ -1,2 +1,13 @@
-<?php echo htmlspecialchars($_POST['name']); ?>씨 안녕하세요.
-당신은 <?php echo (int)$_POST['age']; ?>세입니다.
+<?php echo htmlspecialchars($_POST['name']); ?>씨 안녕하세요.</br>
+<?php echo htmlspecialchars($_POST['depart']); ?>일 <?php echo htmlspecialchars($_POST['genre']); ?>시에 
+<?php echo htmlspecialchars($_POST['chef']); ?>
+아래와 같이 예약 되었습니다.</br>   
+사전 확인 사항:<?php echo htmlspecialchars($_POST['service']); ?>
+<?php
+ $aservice = $_POST['service'];
+
+
+
+변경사항이 있으면 <?php echo (int)$_POST['phone']; ?>으로 연락드리겠습니다. </br>
+
+저에게 "<?php echo htmlspecialchars($_POST['comments']); ?>"이라고 말씀하셨군요</br>
