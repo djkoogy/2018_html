c07804b4b3e0f2cb4c57600e61660fbdb7e57eb5
B. Kook Ha <djkoogy@gmail.com>
2018-04-16 23:39:54 +0000

    수업시작하자

--- action.php
+++ action.php
@@ -1,12 +1,14 @@
 <?php echo htmlspecialchars($_POST['name']); ?>씨 안녕하세요.</br>
 <?php echo htmlspecialchars($_POST['depart']); ?>일 <?php echo htmlspecialchars($_POST['genre']); ?>시에 
-<?php echo htmlspecialchars($_POST['chef']); ?>
+<?php echo htmlspecialchars($_POST['chef']); ?>쉐프님의 요리를 
 아래와 같이 예약 되었습니다.</br>   
-사전 확인 사항:<?php echo htmlspecialchars($_POST['service']); ?>
-<?php
- $aservice = $_POST['service'];
-
-
+사전 확인 사항:
+<?php 
+if(!empty($_POST['service'])){
+	foreach($_POST['service'] as $selected){
+		echo $selected."_";}
+}
+?>을 선택하셨습니다.</br>
 
 변경사항이 있으면 <?php echo (int)$_POST['phone']; ?>으로 연락드리겠습니다. </br>
 
